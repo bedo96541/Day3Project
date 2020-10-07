b0a70e7a70b506f3a381041c0e8c0e1ff2446b8e
BedrettinErkan <bedrettinerkan123@gmail.com>
2020-10-07 17:37:02 +0300

    added more practice

--- src/test/java/AwasomeTest.java
+++ src/test/java/AwasomeTest.java
@@ -4,4 +4,9 @@ public class AwasomeTest {
     // green means under staging area
     // grey in .idea folder it s ignored
     // when th ecolor is normal , it means its commited bo changes
+
+    //I just pulled whe new change from GitHub that i did not have locally
+    //and now my project is in sync with remote repository
+
+    //Now i made more awasome changes and i am about to push this back to Github
 }
