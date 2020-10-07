0db0cde2f93a852b740048ba4c496d748ecdf612
BedrettinErkan <bedrettinerkan123@gmail.com>
2020-10-07 17:25:13 +0300

    Added new file for awasome feature

---
+++ src/test/java/AwasomeTest.java
@@ -0,0 +1,7 @@
+public class AwasomeTest {
+
+    // red means it s untracked
+    // green means under staging area
+    // grey in .idea folder it s ignored
+    // when th ecolor is normal , it means its commited bo changes
+}
